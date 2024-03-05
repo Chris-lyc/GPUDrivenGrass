1abcd27caaf15796ef5b77ebdea9f72398df25de
Santlance <981103924@qq.com>
2024-03-05 22:13:59 +0800

    ComputerShader处理粒子Demo

--- Assets/ComputeShaderDemo/ComputeShaderDemo.cs
+++ Assets/ComputeShaderDemo/ComputeShaderDemo.cs
@@ -2,6 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+public struct ParticleData
+{
+    public Vector3 pos;//equal to float3 in shader
+    public Color color;//equal to float4 in shader
+}
+
 public class ComputeShaderDemo : MonoBehaviour
 {
 
@@ -11,12 +18,18 @@ public class ComputeShaderDemo : MonoBehaviour
     public ComputeShader computeShader;
     public Material material;
 
+    public int particleCounts;
+
 
     public int InstanceCounts = 1000000;
     public Vector3Int InstanceExtents = new Vector3Int(500, 500, 500);
     public float RandomMaxScaleValue = 5;
 
+    public ComputeBuffer particleDataBuffer;
+
     private Matrix4x4[] instances;
+
+    private int kernelId;
 
     // Start is called before the first frame update
     void Start()
@@ -32,6 +45,13 @@ public class ComputeShaderDemo : MonoBehaviour
         computeShader.SetTexture(kernelIndex, "Result", mRenderTexture);
 
         computeShader.Dispatch(kernelIndex, 256 / 8, 256 / 8, 1);
+
+        particleDataBuffer = new ComputeBuffer(particleCounts, 28);
+        ParticleData[] particleDatas = new ParticleData[particleCounts];
+        particleDataBuffer.SetData(particleDatas);
+        kernelId = computeShader.FindKernel("UpdateParticle");
+
+
     }
 
     private Matrix4x4[] RandomGenerateInstances(int instanceCount, Vector3Int instanceExtents, float maxScale)
@@ -58,6 +78,22 @@ public class ComputeShaderDemo : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        computeShader.SetBuffer(kernelId, "ParticleBuffer", particleDataBuffer);
+        computeShader.SetFloat("Time", Time.time);
+        computeShader.Dispatch(kernelId, particleCounts / 1000, 1, 1);
+        // here,computeShader store the result from gpu to particleDataBuffer in cpu, then pass to vf shader
+        material.SetBuffer("_particleDataBuffer", particleDataBuffer);
+    }
+
+    void OnRenderObject()
+    {
+        material.SetPass(0);
+        Graphics.DrawProceduralNow(MeshTopology.Points, particleCounts);
+    }
+
+    void OnDestroy()
+    {
+        particleDataBuffer.Release();
+        particleDataBuffer = null;
     }
 }
